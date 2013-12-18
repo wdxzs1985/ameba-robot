9e5cc4365b17f15932c7b285a7be4695c0364749
wdxzs1985 <wdxzs1985@gmail.com>
2013-12-18 23:49:39 +0900

    升级版本号 0.6.0

--- java/src/robot/tnk47/Tnk47Robot.java
+++ java/src/robot/tnk47/Tnk47Robot.java
@@ -34,7 +34,7 @@ public class Tnk47Robot extends AbstractRobot {
 
     public static final String HOST = "http://tnk47.ameba.jp";
 
-    public static final String VERSION = "天下自动脚本  0.5.4";
+    public static final String VERSION = "天下自动脚本  0.6.0";
 
     @Override
     public void init() {
