0a2725333f86c811207b33ac3a5e1e9e4acebe43
wdxzs1985 <wdxzs1985@gmail.com>
2013-12-21 14:53:53 +0900

    sleep

--- java/src/robot/AbstractEventHandler.java
+++ java/src/robot/AbstractEventHandler.java
@@ -15,78 +15,83 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.message.BasicNameValuePair;
 
 public abstract class AbstractEventHandler<T extends AbstractRobot> implements
-		EventHandler {
+        EventHandler {
 
-	protected final Log log;
-	protected final T robot;
-	private final int requestDelay;
+    protected final Log log;
+    protected final T robot;
+    private final int requestDelay;
 
-	public AbstractEventHandler(final T robot) {
-		this.robot = robot;
-		this.log = LogFactory.getLog(this.getClass());
-		this.requestDelay = robot.getRequestDelay();
-	}
+    public AbstractEventHandler(final T robot) {
+        this.robot = robot;
+        this.log = LogFactory.getLog(this.getClass());
+        this.requestDelay = robot.getRequestDelay();
+    }
 
-	@Override
-	public final void handle() {
-		this.before();
-		this.robot.dispatch(this.handleIt());
-		this.after();
-	}
+    @Override
+    public final void handle() {
+        this.before();
+        try {
+            this.robot.dispatch(this.handleIt());
+        } catch (final Exception e) {
+            final String message = e.getMessage();
+            this.log.error("发生异常: " + message, e);
+        } finally {
+            this.after();
+        }
+    }
 
-	protected void before() {
-	}
+    protected void before() {
+    }
 
-	protected abstract String handleIt();
+    protected abstract String handleIt();
 
-	protected void after() {
-		this.sleep();
-	}
+    protected void after() {
+        this.sleep();
+    }
 
-	protected String httpGet(final String path) {
-		final String url = this.robot.buildPath(path);
-		final String html = this.robot.getHttpClient().getForHtml(url);
-		this.robot.getHttpClient().setReferer(url);
-		return html.replaceAll("\\r\\n[\\t\\s]*|\\r[\\t\\s]*|\\n[\\t\\s]*", "");
-	}
+    protected String httpGet(final String path) {
+        final String url = this.robot.buildPath(path);
+        final String html = this.robot.getHttpClient().getForHtml(url);
+        this.robot.getHttpClient().setReferer(url);
+        return html.replaceAll("\\r\\n[\\t\\s]*|\\r[\\t\\s]*|\\n[\\t\\s]*", "");
+    }
 
-	protected String httpPost(final String path,
-			final List<BasicNameValuePair> nvps) {
-		final String url = this.robot.buildPath(path);
-		final String html = this.robot.getHttpClient().postForHtml(url, nvps);
-		this.robot.getHttpClient().setReferer(url);
-		return html.replaceAll("\\r\\n[\\t\\s]*|\\r[\\t\\s]*|\\n[\\t\\s]*", "");
-	}
+    protected String httpPost(final String path,
+                              final List<BasicNameValuePair> nvps) {
+        final String url = this.robot.buildPath(path);
+        final String html = this.robot.getHttpClient().postForHtml(url, nvps);
+        this.robot.getHttpClient().setReferer(url);
+        return html.replaceAll("\\r\\n[\\t\\s]*|\\r[\\t\\s]*|\\n[\\t\\s]*", "");
+    }
 
-	protected JSONObject httpGetJSON(final String path) {
-		final String url = this.robot.buildPath(path);
-		final String html = this.robot.getHttpClient().getForHtml(url);
-		return JSONObject.fromObject(html);
-	}
+    protected JSONObject httpGetJSON(final String path) {
+        final String url = this.robot.buildPath(path);
+        final String html = this.robot.getHttpClient().getForHtml(url);
+        return JSONObject.fromObject(html);
+    }
 
-	protected JSONObject httpPostJSON(final String path,
-			final List<BasicNameValuePair> nvps) {
-		final String url = this.robot.buildPath(path);
-		final String html = this.robot.getHttpClient().postForHtml(url, nvps);
-		return JSONObject.fromObject(html);
-	}
+    protected JSONObject httpPostJSON(final String path,
+                                      final List<BasicNameValuePair> nvps) {
+        final String url = this.robot.buildPath(path);
+        final String html = this.robot.getHttpClient().postForHtml(url, nvps);
+        return JSONObject.fromObject(html);
+    }
 
-	protected List<BasicNameValuePair> createNameValuePairs() {
-		return new LinkedList<BasicNameValuePair>();
-	}
+    protected List<BasicNameValuePair> createNameValuePairs() {
+        return new LinkedList<BasicNameValuePair>();
+    }
 
-	protected boolean is(final String funcName) {
-		final Map<String, Object> session = this.robot.getSession();
-		final boolean enable = (Boolean) session.get(funcName);
-		return enable;
-	}
+    protected boolean is(final String funcName) {
+        final Map<String, Object> session = this.robot.getSession();
+        final boolean enable = (Boolean) session.get(funcName);
+        return enable;
+    }
 
-	protected void sleep() {
-		final int sleepTime = this.requestDelay
-				+ RandomUtils.nextInt(this.requestDelay);
-		try {
-			Thread.sleep(sleepTime * 1000);
-		} catch (final InterruptedException e) {
-		}
-	}
+    protected void sleep() {
+        final int sleepTime = this.requestDelay + RandomUtils.nextInt(this.requestDelay);
+        try {
+            Thread.sleep(sleepTime * 1000);
+        } catch (final InterruptedException e) {
+        }
+    }
 }
--- java/src/robot/AbstractRobot.java
+++ java/src/robot/AbstractRobot.java
@@ -13,112 +13,105 @@ import common.CommonHttpClient;
 
 public abstract class AbstractRobot implements Robot, Runnable {
 
-	protected final Log log = LogFactory.getLog(this.getClass());
-	private final Map<String, Object> session = Collections
-			.synchronizedMap(new HashMap<String, Object>());
-	private final Map<String, EventHandler> handlerMapping = new HashMap<String, EventHandler>();
+    protected final Log log = LogFactory.getLog(this.getClass());
+    private final Map<String, Object> session = Collections.synchronizedMap(new HashMap<String, Object>());
+    private final Map<String, EventHandler> handlerMapping = new HashMap<String, EventHandler>();
 
-	private CommonHttpClient httpClient = null;
-	private EventHandler nextHandler = null;
-	private Properties config = null;
-	private boolean running = false;
+    private CommonHttpClient httpClient = null;
+    private EventHandler nextHandler = null;
+    private Properties config = null;
+    private boolean running = false;
 
-	protected void registerHandler(final String path, final EventHandler handler) {
-		this.handlerMapping.put(path, handler);
-	}
+    protected void registerHandler(final String path, final EventHandler handler) {
+        this.handlerMapping.put(path, handler);
+    }
 
-	@Override
-	public void run() {
-		this.setRunning(true);
-		this.dispatch("/");
-		while (this.isRunning() && this.nextHandler != null) {
-			final EventHandler currEventHandler = this.nextHandler;
-			this.nextHandler = null;
-			try {
-				currEventHandler.handle();
-			} catch (final Exception e) {
-				final String message = e.getMessage();
-				this.log.error("发生异常: " + message, e);
-			} finally {
-				this.httpClient.saveCookie();
-			}
-		}
-	}
+    @Override
+    public void run() {
+        this.setRunning(true);
+        this.dispatch("/");
+        while (this.isRunning() && this.nextHandler != null) {
+            final EventHandler currEventHandler = this.nextHandler;
+            this.nextHandler = null;
+            currEventHandler.handle();
+            this.httpClient.saveCookie();
+        }
+    }
 
-	@Override
-	public void dispatch(final String event) {
-		if (StringUtils.equals(event, "/exit")) {
-			this.setRunning(false);
-			if (this.log.isInfoEnabled()) {
-				final int delay = this.getScheduleDelay();
-				this.log.info(String.format("休息%d分钟", delay));
-			}
-		} else {
-			this.nextHandler = this.handlerMapping.get(event);
-			if (this.nextHandler == null) {
-				if (this.log.isWarnEnabled()) {
-					this.log.warn(String.format("未知方法[%s]", event));
-				}
-			}
-		}
-	}
+    @Override
+    public void dispatch(final String event) {
+        if (StringUtils.equals(event, "/exit")) {
+            this.setRunning(false);
+            if (this.log.isInfoEnabled()) {
+                final int delay = this.getScheduleDelay();
+                this.log.info(String.format("休息%d分钟", delay));
+            }
+        } else {
+            this.nextHandler = this.handlerMapping.get(event);
+            if (this.nextHandler == null) {
+                if (this.log.isWarnEnabled()) {
+                    this.log.warn(String.format("未知方法[%s]", event));
+                }
+            }
+        }
+    }
 
-	public String buildPath(final String path) {
-		final String host = this.getHost();
-		return host + path;
-	}
+    public String buildPath(final String path) {
+        final String host = this.getHost();
+        return host + path;
+    }
 
-	public Map<String, Object> getSession() {
-		return this.session;
-	}
+    public Map<String, Object> getSession() {
+        return this.session;
+    }
 
-	public CommonHttpClient getHttpClient() {
-		return this.httpClient;
-	}
+    public CommonHttpClient getHttpClient() {
+        return this.httpClient;
+    }
 
-	public void setHttpClient(final CommonHttpClient httpClient) {
-		this.httpClient = httpClient;
-	}
+    public void setHttpClient(final CommonHttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
 
-	public Properties getConfig() {
-		return this.config;
-	}
+    public Properties getConfig() {
+        return this.config;
+    }
 
-	public void setConfig(final Properties config) {
-		this.config = config;
-	}
+    public void setConfig(final Properties config) {
+        this.config = config;
+    }
 
-	protected abstract String getHost();
+    protected abstract String getHost();
 
-	public int getRequestDelay() {
-		final String key = "Robot.requestDelay";
-		final String value = this.getConfig().getProperty(key, "3");
-		return Integer.valueOf(value);
-	}
+    public int getRequestDelay() {
+        final String key = "Robot.requestDelay";
+        final String value = this.getConfig().getProperty(key, "3");
+        return Integer.valueOf(value);
+    }
 
-	public int getScheduleDelay() {
-		final String key = "Robot.scheduleDelay";
-		final String value = this.getConfig().getProperty(key, "5");
-		return Integer.valueOf(value);
-	}
+    public int getScheduleDelay() {
+        final String key = "Robot.scheduleDelay";
+        final String value = this.getConfig().getProperty(key, "5");
+        return Integer.valueOf(value);
+    }
 
-	public String getUsername() {
-		final String key = "Robot.username";
-		final String value = this.getConfig().getProperty(key);
-		return value;
-	}
+    public String getUsername() {
+        final String key = "Robot.username";
+        final String value = this.getConfig().getProperty(key);
+        return value;
+    }
 
-	public String getPassword() {
-		final String key = "Robot.password";
-		final String value = this.getConfig().getProperty(key);
-		return value;
-	}
+    public String getPassword() {
+        final String key = "Robot.password";
+        final String value = this.getConfig().getProperty(key);
+        return value;
+    }
 
-	public boolean isRunning() {
-		return this.running;
-	}
+    public boolean isRunning() {
+        return this.running;
+    }
 
-	public void setRunning(final boolean running) {
-		this.running = running;
-	}
+    public void setRunning(final boolean running) {
+        this.running = running;
+    }
 }
