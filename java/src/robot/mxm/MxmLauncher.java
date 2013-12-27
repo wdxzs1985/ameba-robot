ea97f30420931b43620093059d2dc9b86e623d7e
wdxzs1985 <wdxzs1985@gmail.com>
2013-12-27 11:28:42 +0900

    reset

--- java/src/robot/mxm/MxmLauncher.java
+++ java/src/robot/mxm/MxmLauncher.java
@@ -4,34 +4,29 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import robot.Launcher;
 
 public class MxmLauncher extends Launcher {
 
-	private final ScheduledExecutorService executor = Executors
-			.newScheduledThreadPool(1);
-	private final Log log = LogFactory.getLog(this.getClass());
+    public static void main(final String[] args) {
+        final String setup = args.length > 0 ? args[0] : "setup.txt";
+        final MxmLauncher launcher = new MxmLauncher();
+        launcher.init(setup);
+        launcher.launch();
+    }
 
-	public static void main(final String[] args) {
-		final String setup = args.length > 0 ? args[0] : "setup.txt";
-		final MxmLauncher launcher = new MxmLauncher();
-		launcher.init(setup);
-		launcher.launch();
-	}
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
-	public void launch() {
-		if (this.log.isInfoEnabled()) {
-			this.log.info(MxmRobot.VERSION);
-		}
-		final MxmRobot robot = new MxmRobot();
-		robot.setConfig(this.config);
-		robot.setHttpClient(this.httpClient);
-		robot.init();
-
-		int delay = robot.getScheduleDelay();
-		this.executor.scheduleWithFixedDelay(robot, 0, delay, TimeUnit.MINUTES);
-	}
+    public void launch() {
+        if (this.log.isInfoEnabled()) {
+            this.log.info(MxmRobot.VERSION);
+        }
+        final MxmRobot robot = new MxmRobot();
+        robot.setConfig(this.config);
+        robot.setHttpClient(this.httpClient);
+        robot.init();
+        robot.reset();
+        int delay = robot.getScheduleDelay();
+        this.executor.scheduleWithFixedDelay(robot, 0, delay, TimeUnit.MINUTES);
+    }
 }
