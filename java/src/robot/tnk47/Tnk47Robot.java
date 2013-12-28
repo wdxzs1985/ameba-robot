82b0bd8b10e8ef0d3eb83f84d6bf24e80451090c
wdxzs1985 <wdxzs1985@gmail.com>
2013-12-29 00:59:05 +0900

    0.7.5

--- java/src/robot/tnk47/Tnk47Robot.java
+++ java/src/robot/tnk47/Tnk47Robot.java
@@ -44,7 +44,7 @@ public class Tnk47Robot extends AbstractRobot {
 
     public static final String HOST = "http://tnk47.ameba.jp";
 
-    public static final String VERSION = "天下自动脚本  0.7.4";
+    public static final String VERSION = "天下自动脚本  0.7.5";
 
     @Override
     public void init() {
