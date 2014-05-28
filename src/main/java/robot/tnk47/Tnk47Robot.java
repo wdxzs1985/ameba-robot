c65c66d8e4e9c28f216f40c058d7ed07435008da
wdxzs1985 <wdxzs1985@gmail.com>
2014-05-28 19:49:28 +0900

    修复箱子抽

--- src/main/java/robot/tnk47/Tnk47Robot.java
+++ src/main/java/robot/tnk47/Tnk47Robot.java
@@ -70,7 +70,7 @@ public class Tnk47Robot extends AbstractRobot {
 
     public static final String HOST = "http://tnk47.ameba.jp";
 
-    public static final String VERSION = "天下自动脚本  11.3";
+    public static final String VERSION = "天下自动脚本  11.4";
 
     @Override
     public void initHandlers() {
